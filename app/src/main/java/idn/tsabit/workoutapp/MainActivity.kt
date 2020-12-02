78e9ff60e4e9150fe13c52b921ba930a6da698e3
tsabitfarel <tsabitzfarel@gmail.com>
2020-12-02 11:14:38 +0800

    Initial commit

---
+++ app/src/main/java/idn/tsabit/workoutapp/MainActivity.kt
@@ -0,0 +1,29 @@
+package idn.tsabit.workoutapp
+
+import android.content.Intent
+import androidx.appcompat.app.AppCompatActivity
+import android.os.Bundle
+import android.widget.Toast
+import kotlinx.android.synthetic.main.activity_main.*
+
+class MainActivity : AppCompatActivity() {
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+
+        linearStart.setOnClickListener {
+            val intent = Intent(this, ExerciseActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnBmi.setOnClickListener {
+            val intent = Intent(this, BmiCalculatorActivity::class.java)
+            startActivity(intent)
+        }
+
+        btnHistory.setOnClickListener {
+            val intent = Intent(this, HistoryActivity::class.java)
+            startActivity(intent)
+        }
+    }
+}
